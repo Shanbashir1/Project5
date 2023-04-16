b182eeeb5215d41c9dfc92eb8d4942be8c562b3c
Shanbashir1 <shanbashir1@hotmail.com>
2023-04-16 12:56:59 +0000

    Added checkout html template

--- prestige_watches/settings.py
+++ prestige_watches/settings.py
@@ -81,12 +81,12 @@ TEMPLATES = [
                 'django.template.context_processors.request',  # required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'django.template.context_processors.media.',
+                'django.template.context_processors.media',
                 'bag.contexts.bag_contents',
             ],
             'builtins': [
-                'crsipy_forms.templatetags.crispy_forms_tags'
-                'crsipy_forms.templatetags.crispy_forms_fields'
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
             ]
         },
     },
