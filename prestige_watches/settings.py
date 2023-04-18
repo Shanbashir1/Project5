f7d9a02b869af38bf2218a3f99a3df2d3eb267a3
Shanbashir1 <shanbashir1@hotmail.com>
2023-04-18 17:27:22 +0000

    Fixed bug in checkout, static. js which was not allowing order to display in admin

--- prestige_watches/settings.py
+++ prestige_watches/settings.py
@@ -127,7 +127,7 @@ WSGI_APPLICATION = 'prestige_watches.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
@@ -169,7 +169,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
