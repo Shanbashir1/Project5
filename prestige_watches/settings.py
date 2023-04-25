2d92143fb3a532631da50b1eb3ef67b766ef6705
Shanbashir1 <shanbashir1@hotmail.com>
2023-04-25 10:32:43 +0000

    Added confirmation_emails

--- prestige_watches/settings.py
+++ prestige_watches/settings.py
@@ -183,3 +183,6 @@ STRIPE_CURRENCY = 'gbp'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
+
+# Sending Confirmation Emails
+DEFAULT_FROM_EMAIL = "prestigewatches@example.com"
