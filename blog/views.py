8a8627e7bdaa80522a34fa972f6a1d09a0f6b387
Shanbashir1 <shanbashir1@hotmail.com>
2023-04-27 11:01:42 +0000

    Create urls and template for blog app

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from . import views
+from .models import Post, Comment
 
-# Create your views here.
+
+def post_list(request):
+    """View to post a blog"""
+    post_list = Post.objects.all()
+
+    return render(request, 'blog/blog.html')
