1b18b6f720cdad6bef3d361d62358f6fe50c894a
Shanbashir1 <shanbashir1@hotmail.com>
2023-05-03 11:59:56 +0000

    Finished footer and created About us page

--- contactus/urls.py
+++ contactus/urls.py
@@ -4,4 +4,5 @@ from . import views
 # Url Patterns for Contacts Page
 urlpatterns = [
     path('contactus/', views.ContactUs.as_view(), name='contactus'),
+    path('aboutus/', views.AboutUs.as_view(), name='aboutus'),
 ]
