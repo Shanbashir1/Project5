5585750e5dd0f3a64a398d6800769f550dcfed47
Shanbashir1 <shanbashir1@hotmail.com>
2023-05-04 17:19:30 +0000

    Testing for errors on deployment

--- prestige_watches/settings.py
+++ prestige_watches/settings.py
@@ -18,7 +18,7 @@ if os.path.exists("env.py"):
     import env
 
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)	
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
